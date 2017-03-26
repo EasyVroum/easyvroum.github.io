b6f468fd7e5f752cec02318f477ce533da9d33ed
AlisCode <oliv.pinon@gmail.com>
2017-03-26 21:53:31 +0200

    autoloop video

--- js/app.js
+++ js/app.js
@@ -17,6 +17,23 @@ $(function() {
 		$(document.body).animate({'scrollTop': $("#nos-solutions").offset().top}, 1000);
 	});
 
+	var phonevideo = $("#phone-video")[0];
 
+	var waypointVideos = new Waypoint( {
+		element: $("#nos-solutions"),
+		handler: function()
+		{
+			if(phonevideo.paused)
+			{
+				phonevideo.play();
+			}
+		}
+	});
 
+	window.setInterval(function() {
+		if(phonevideo.paused)
+		{
+			phonevideo.play();
+		}
+	}, 2000);
 });
